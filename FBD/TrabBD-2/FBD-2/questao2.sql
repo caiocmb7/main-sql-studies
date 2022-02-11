2817ab083d5c33b7865801d83b41a5307fc25af7
Caio Barros <caiobarrosufc@gmail.com>
2022-02-11 15:19:09 -0300

    updated questions 2 and 4

--- FBD/TrabBD-2/FBD-2/questao2.sql
+++ FBD/TrabBD-2/FBD-2/questao2.sql
@@ -3,14 +3,8 @@ CREATE VIEW visaoQuestao2 AS
 SELECT
     Alunos.nome AS Nome_aluno,
     Curso.nome AS Nome_curso,
-    COUNT(Disciplina.Cod_disc) AS soma_disciplina,
-    COUNT(Disciplina.qte_creditos) AS soma_creditos,
-    ROUND(AVG(
-        CASE
-            WHEN Aluno_disc.AF IS NULL THEN ((Aluno_disc.PrimeiraAP + Aluno_disc.SegundaAP)/2)
-            ELSE ((Aluno_disc.PrimeiraAP + Aluno_disc.SegundaAP + Aluno_disc.AF)/3)
-        END
-    ),2) as media_global
+    COUNT(DISTINCT Disciplina.Cod_disc) AS soma_disciplina,
+    SUM(Disciplina.qte_creditos) AS soma_creditos
 FROM Disciplina
 INNER JOIN Aluno_disc
     ON Aluno_disc.Cod_disc_fk = Disciplina.Cod_disc
@@ -20,12 +14,9 @@ INNER JOIN Cur_Disc
     ON Cur_Disc.Cod_disc_fk = Disciplina.Cod_disc
 INNER JOIN Curso
     ON Cur_Disc.Cod_curso_fk = Curso.Cod_curso
-GROUP BY Disciplina.Cod_disc;
+WHERE ((Aluno_disc.PrimeiraAP + Aluno_disc.SegundaAP)/2 > 7) OR (((Aluno_disc.PrimeiraAP + Aluno_disc.SegundaAP)/2 + Aluno_disc.AF)/2 > 5)
+GROUP BY Curso.nome, Alunos.nome;
 
 SELECT 
-    Nome_aluno,
-    Nome_curso,
-    soma_disciplina,
-    soma_creditos
-FROM visaoQuestao2
-WHERE media_global > 7;
+    *
+FROM visaoQuestao2;
