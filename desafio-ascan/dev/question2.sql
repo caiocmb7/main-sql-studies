509e375757d8336a4182b6273dc2bb4d902e56ee
Caio Barros <caiobarrosufc@gmail.com>
2022-05-24 16:45:17 -0300

    updated question2

--- desafio-ascan/dev/question2.sql
+++ desafio-ascan/dev/question2.sql
@@ -14,6 +14,15 @@ WHERE
 
 # acaba que não precisa calcular usando o metro quadrado por ele é igual e ele sai da conta na divisão
 
+/*
+
+2. Escreva uma consulta MySQL para:
+
+- Contar o número de imóveis que tiveram aumento de preço em 2016 e o aumento médio do percentual de preço/m² desses imóveis;
+
+- Contar o número de imóveis que tiveram um decréscimo de preço em 2016 e a diminuição média da percentagem de preço/m2 desses imóveis;
+
+*/
 SELECT
     COUNT(*) as number_proprieties,
     ROUND(((price_changes_table.new_price / price_changes_table.old_price) - 1)*100, 2) as increased_percentage_average # increased percentage per square meter
